9836b118a7a3ba09cd1c57dafcbc686d9bd4b0db
K.Kiyokawa <koichi20110068@gmail.com>
2020-11-07 16:02:31 +0900

    feat: add link to nav

--- src/components/organisms/Nav.tsx
+++ src/components/organisms/Nav.tsx
@@ -1,9 +1,18 @@
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import Link from 'next/link'
+import { AppBar, Button, Toolbar, Typography } from '@material-ui/core'
 
 export const Nav = () => (
   <AppBar position="fixed">
     <Toolbar>
       <Typography variant="h6">Next</Typography>
+      <div style={{ marginLeft: 'auto' }}>
+        <Link href="/login">
+          <Button color="inherit">ログイン</Button>
+        </Link>
+        <Link href="/counter">
+          <Button color="inherit">カウンター</Button>
+        </Link>
+      </div>
     </Toolbar>
   </AppBar>
 )
